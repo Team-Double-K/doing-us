c42ee9bf9cf9245797d5853e5b0960c7a66cf526
KNU-K <knukang334@gmail.com>
2023-11-11 19:53:22 +0900

    hofix : 최종 room controller 와 service 완료
    최종 작업끝!

--- backend/controllers/room.controller.js
+++ backend/controllers/room.controller.js
@@ -1,74 +1,128 @@
-const CustomError = require('../helpers/custom_error.helper')
-const RoomService = require('../services/room.service')
+const CustomError = require("../helpers/custom_error.helper");
+const RoomService = require("../services/room.service");
 
 const findRoom = async (req, res, next) => {
   try {
-    const result = await RoomService.findAll()
-    res.send(result)
+    const result = await RoomService.findAll();
+    res.send(result);
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 const findRoomOfUserByUserId = async (req, res, next) => {
   try {
-    const { userId } = req.params
+    const { userId } = req.params;
 
-    const result = await RoomService.findRoomOfUserByUserId(userId)
-    res.send(result)
+    const result = await RoomService.findRoomOfUserByUserId(userId);
+    res.send(result);
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 const findDetailOfRoomOfUserByUserIdAndRoomId = async (req, res, next) => {
   try {
-    const { userId, roomId } = req.params
+    const { userId, roomId } = req.params;
 
-    const result = await RoomService.findDetailOfRoomOfUserByUserIdAndRoomId(userId, roomId)
-    if (!result) throw new CustomError('failed searching data, not exist data', 400)
-    res.send(result)
+    const result = await RoomService.findDetailOfRoomOfUserByUserIdAndRoomId(
+      userId,
+      roomId
+    );
+    if (!result)
+      throw new CustomError("failed searching data, not exist data", 400);
+    res.send(result);
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 const createRoomByUserId = async (req, res, next) => {
   try {
-    const { userId } = req.params
-    const roomInfo = req.body
+    const { userId } = req.params;
+    const roomInfo = req.body;
     if (!(await RoomService.createRoomByUserId(userId, roomInfo)))
-      throw new CustomError('expect error, when create room', 400)
+      throw new CustomError("expect error, when create room", 400);
 
     res.send({
       state: 1,
-      msg: 'successful to create room that is your owns',
-    })
+      msg: "successful to create room that is your owns",
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 const joinRoomByUserIdAndRoomId = async (req, res, next) => {
   try {
-    const { userId, roomId } = req.params
+    const { userId, roomId } = req.params;
     if (!(await RoomService.joinRoomByUserIdAndRoomId(userId, roomId)))
-      throw new CustomError('error joining room, check roomId or userId', 400)
+      throw new CustomError("error joining room, check roomId or userId", 400);
 
     res.send({
       state: 1,
-      msg: 'successful joining room ',
-    })
+      msg: "successful joining room ",
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
-const deleteRoomByUserIdAndRoomId = async (req, res, next) => {}
-const updateOrExitRoomByUserIdAndRoomId = async (req, res, next) => {}
+};
+
+/**
+ * TODO: exit room and delete room
+ */
+const deleteOrExitRoomByUserIdAndRoomId = async (req, res, next) => {
+  try {
+    const { userId, roomId } = req.params;
+    if (await RoomService.isOwnerForRoom(userId, roomId)) {
+      //ownerÀÏ ¶§,
+      if (!(await RoomService.deleteRoomByUserIdAndRoomId(userId, roomId)))
+        throw new CustomError("It was not processed properly.", 401);
+
+      res.send({
+        state: 1,
+        msg: "successful to delete room",
+      });
+    } else {
+      //owner¾Æ´Ò¶§
+      if (!(await RoomService.exitRoomByUserIdAndRoomId(userId, roomId)))
+        throw new CustomError("It was not processed properly.", 401);
+
+      res.send({
+        state: 1,
+        msg: "successful to exit room",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateRoomByUserIdAndRoomId = async (req, res, next) => {
+  try {
+    const { userId, roomId } = req.params;
+    const roomInfo = req.body;
+    if (!(await RoomService.isOwnerForRoom(userId, roomId)))
+      throw new CustomError("wrong access ..!", 404);
+
+    if (
+      !(await RoomService.updateRoomByUserIdAndRoomId(userId, roomId, roomInfo))
+    )
+      throw new CustomError("It was not processed properly.", 401);
+
+    res.send({
+      state: 1,
+      msg: "successful to update room",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
   findRoom: findRoom,
   findRoomOfUserByUserId: findRoomOfUserByUserId,
-  findDetailOfRoomOfUserByUserIdAndRoomId: findDetailOfRoomOfUserByUserIdAndRoomId,
+  findDetailOfRoomOfUserByUserIdAndRoomId:
+    findDetailOfRoomOfUserByUserIdAndRoomId,
   createRoomByUserId: createRoomByUserId,
   joinRoomByUserIdAndRoomId: joinRoomByUserIdAndRoomId,
-  deleteRoomByUserIdAndRoomId: deleteRoomByUserIdAndRoomId,
-  updateOrExitRoomByUserIdAndRoomId: updateOrExitRoomByUserIdAndRoomId,
-}
+  deleteOrExitRoomByUserIdAndRoomId: deleteOrExitRoomByUserIdAndRoomId,
+  updateRoomByUserIdAndRoomId: updateRoomByUserIdAndRoomId,
+};
